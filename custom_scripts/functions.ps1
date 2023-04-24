54d79790af380be88ed03a43a9570a3d24eaad27
Ben Schultz <bschultz@email.com>
2023-04-24 16:39:19 -0500

    Broke out files into separate directories.

--- custom_scripts/functions.ps1
+++ custom_scripts/functions.ps1
@@ -1,3 +1,26 @@
+#Only display the last two directories in the prompt
+function prompt {
+	Write-Host (Split-Path -Path (Get-Location) -Leaf)">"
+}
+
+function rename($file) {
+	$newname = Read-Host "Enter new file name:"
+	Rename-Item -Path $file -NewName $newname
+	Write-Host "Renamed to $newname"
+}	
+
+function getpath($file) {
+	if (!$file) {
+		Write-Host "Error: No source file provided."
+		return
+	}
+	$dir = (Get-Item $file).DirectoryName
+	$file = (Get-Item $file).Name
+	$path = Join-Path $dir $file
+	Set-Clipboard $path
+	Write-Host "Copied $path to clipboard"
+}
+
 function man($cmd) {
     Get-Help $cmd -full
 }
