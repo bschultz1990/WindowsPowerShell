ebc28c816ef871f9cbf9cecdd497bddfa33c417b
= <=>
2023-06-10 15:46:35 -0500

    Refactor to use npm package of trash

--- custom_scripts/functions.ps1
+++ custom_scripts/functions.ps1
@@ -1,7 +1,36 @@
-$trash_folder = "~/Desktop/trash"
-if (-not(Test-Path "$trash_folder")) {
-	mkdir "$trash_folder"
-}
+# Create a config file if it doesn't exists and set the config directory.
+if (-not (Test-Path -Path $PROFILE)) {
+	New-Item -Path $PROFILE -Type File -Force
+	}
+($PROFILE) -match "(.+?)Microsoft.PowerShell_profile.ps1"
+$pwshconfig = $Matches[1]
+
+# Set neovim config directory
+$nvimconfig = "~\AppData\Local\nvim\"
+if (-not (Test-Path -Path $nvimconfig)) {
+	New-Item -Path ("$nvimconfig", "init.lua" -join "") -Type File -Force
+	}
+
+# $trash_folder = "~/Documents/trash"
+# if (-not(Test-Path "$trash_folder")) {
+# 	mkdir "$trash_folder"
+# 	Write-Host "Trash folder created at: $trash_folder"
+# }
+
+# function trash ($file) {
+#   Move-Item -Path $file -Destination $trash_folder -Force
+# }
+
+# function trashempty {
+#   Remove-Item -Path $trash_folder\*
+#   Write-Host "Trash emptied."
+# }
+
+# function trashview {
+#   $back = $PWD
+#   cd $trash_folder
+#   ls
+# }
 
 function rename ($file) {
 	$destination = Read-Host "Enter a new name:"
@@ -132,20 +161,8 @@ function touch($file) {
 }
 
 function mkdir ($dir) {
-  New-Item -ItemType Directory -Path $dir -Force
+if (-not (Test-Path -Path $PROFILE)) {
+	New-Item -Path $PROFILE -Type File -Force
+	}
 }
 
-function trash ($file) {
-  Move-Item -Path $file -Destination $trash_folder -Force
-}
-
-function trashempty {
-  Remove-Item -Path $trash_folder\*
-  Write-Host "Trash emptied."
-}
-
-function trashview {
-  $back = $PWD
-  cd $trash_folder
-  ls
-}
