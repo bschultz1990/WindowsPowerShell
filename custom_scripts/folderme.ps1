a6de08ccbcafa17b0bf59521dfbb1952b2a9063f
Ben Schultz <Ben_Schultz@vfc.com>
2023-06-12 09:41:13 -0500

    Refactor olderme to handle duplicate matches.

--- custom_scripts/folderme.ps1
+++ custom_scripts/folderme.ps1
@@ -1,7 +1,12 @@
 function folderme {
-    $ext_input = Read-Host "Enter file extension to look for starting with *.:"
-    $ext = 
-    $files = Get-ChildItem -Path .\ -Filter $ext -File
+    param (
+        [Parameter(Mandatory=$true,
+        position=0)]
+        [String]$FolderPath
+    )
+    $ext_input = Read-Host "Enter file extension to look for:"
+    $ext = "*.", $ext_input -join ""
+    $files = Get-ChildItem -Path $FolderPath -Filter $ext -File
 
     $prefix = Read-Host "What is before your match?"
     $suffix = Read-Host "What is immediately after your match?"
@@ -11,18 +16,27 @@ function folderme {
     $prename = Read-Host "What would you like before your new folder name? (leave blank to skip)"
     $postname = Read-Host "How about after? (leave blank to skip)"
 
-    foreach ($file in $files) {
-        $fileName = $file.Name
-        if ($fileName -match "$regex") {
-            $digits = $Matches[1]
-            $newFolderName = "$prename""$digits""$postname"
-            $newFolderPath = Join-Path -Path .\ -ChildPath $newFolderName
+$uniqueMatches = @{}  # create hashtable to store unique matches
 
+foreach ($file in $files) {
+    $fileName = $file.Name
+    if ($fileName -match "$regex") {
+        $digits = $Matches[1]
+        $newFolderName = "$prename", "$digits", "$postname" -join ""
+        $newFolderPath = Join-Path -Path $FolderPath -ChildPath $newFolderName
+
+        # check if folder name already exists in hashtable
+        if ($uniqueMatches.ContainsKey($newFolderName)) {
+            $newFolderPath = $uniqueMatches[$newFolderName]  # use existing folder path
+        } else {
+            # create new folder and add to hashtable
             if (-not (Test-Path -Path $newFolderPath)) {
                 New-Item -ItemType Directory -Path $newFolderPath | Out-Null
             }
-
-            Move-Item -Path $file.FullName -Destination $newFolderPath
+            $uniqueMatches[$newFolderName] = $newFolderPath
         }
+
+        Move-Item -Path $file.FullName -Destination $newFolderPath
     }
+}
 }
