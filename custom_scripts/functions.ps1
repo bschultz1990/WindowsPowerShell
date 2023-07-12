98c48f8e4ab310bfde1d51b731f83c5a1a0b7ae7
Ben Schultz <Ben_Schultz@vfc.com>
2023-07-12 16:42:28 -0500

    Add more functionality to zip function

--- custom_scripts/functions.ps1
+++ custom_scripts/functions.ps1
@@ -1,44 +1,44 @@
 function test {
-Write-Host "It worked!"
+  Write-Host "It worked!"
 }
 
 function trash {
-[CmdletBinding()]
-param (
-[Parameter(Mandatory=$true, Position=0, ValueFromRemainingArguments=$true)]
-[String[]]$files
-)
-Remove-ItemSafely $files
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true, Position = 0, ValueFromRemainingArguments = $true)]
+    [String[]]$files
+  )
+  Remove-ItemSafely $files
 }
 
 function fzo {
-Invoke-Item $(fzf)
+  Invoke-Item $(fzf)
 }
 
 
 function doctypes {
-clear
-Write-Host "=== Document Types ===`n"
-"4200 = Factory PO (not specific)`n"+
-"1001 = Sales Doc`n"+
-"501_ = External ID of Delivery Note`n"+
-"V000 = External ID of Delivery Note`n"+
-"352_ = Billing Document / Invoice (100% specific)`n"
+  clear
+  Write-Host "=== Document Types ===`n"
+  "4200 = Factory PO (not specific)`n" +
+  "1001 = Sales Doc`n" +
+  "501_ = External ID of Delivery Note`n" +
+  "V000 = External ID of Delivery Note`n" +
+  "352_ = Billing Document / Invoice (100% specific)`n"
 }
 
 function checklist {
-clear
-Write-Host "=== Invoice Checklist ===`n"
-"[ ] Run Python`n"+
-"[ ] NGC Numbers moved on all pages`n"+
-"[ ] Delete footers from middle pages`n"+
-"[ ] Page numbers correct`n"+
-"[ ] Total value`n"+
-"[ ] Total Qty/Pcs from official PL`n"+
-"[ ] Total Cartons/Pkg from official PL`n"+
-"[ ] Net Wt. from official PL`n"+
-"[ ] Gross wt. from BL or AWB`n"+
-"[ ] Add 'Confirmed BJS' to inovice file and send`n"
+  clear
+  Write-Host "=== Invoice Checklist ===`n"
+  "[ ] Run Python`n" +
+  "[ ] NGC Numbers moved on all pages`n" +
+  "[ ] Delete footers from middle pages`n" +
+  "[ ] Page numbers correct`n" +
+  "[ ] Total value`n" +
+  "[ ] Total Qty/Pcs from official PL`n" +
+  "[ ] Total Cartons/Pkg from official PL`n" +
+  "[ ] Net Wt. from official PL`n" +
+  "[ ] Gross wt. from BL or AWB`n" +
+  "[ ] Add 'Confirmed BJS' to inovice file and send`n"
 }
 
 ##FIXME
@@ -47,193 +47,189 @@ Write-Host "=== Invoice Checklist ===`n"
 #         $dir = $regex$(fzf)
 #           Write-Host $dir
 #         }
-
+  
 function brands {
-Set-Clipboard "VANS; ALTRA; CORP_SERV; DICKIES; ICEBREAKER; JANSPORT; NORTHFACE; OA_CLEAR; OA_COAL; PANAMA; SMARTWOOL; SUPREME; TIMBERLAND; VANSCASA"
-Write-Host "Brands copied!"
+  Set-Clipboard "VANS; ALTRA; CORP_SERV; DICKIES; ICEBREAKER; JANSPORT; NORTHFACE; OA_CLEAR; OA_COAL; PANAMA; SMARTWOOL; SUPREME; TIMBERLAND; VANSCASA"
+  Write-Host "Brands copied!"
 }
-
-function dc ($ngc) {
-$url = "https://vfc.sharepoint.com/sites/AppletonINLDocuments/_layouts/15/search.aspx/siteall?q="+"$ngc"
-Write-Host $url
+  
+function docs ($ngc) {
+  $url = "https://vfc.sharepoint.com/sites/AppletonINLDocuments/_layouts/15/search.aspx/siteall?q=" + "$ngc"
+  start $url
 }
-
+  
 function rename ($file) {
-# Set-Clipboard $file
-$destination = Read-Host "Enter a new name"
-Rename-Item -Path "$file" -NewName "$destination"
-Set-Clipboard $destination
-Write-Host "$destination copied!"
+  $destination = Read-Host "Enter a new name"
+  Rename-Item -Path "$file" -NewName "$destination"
+  # Set-Clipboard $destination
+  # Write-Host "$destination copied!"
 }
-
+  
 function cwd {
-Set-Clipboard (getdir)
+  Set-Clipboard (getdir)
 }
-
-# function combine($file1, $file2) {
-# $out_dir = getdir
-# $name = Read-Host "Output file name?"
-# $file1, $file2 | Merge-PDF -OutputPath $out_dir\$name.pdf -Force
-# }
-
 function zip { 
   param (
     [CmdletBinding()]
-    [Parameter(Mandatory=$true, position=0, ValueFromRemainingArguments=$true)]
+    [Parameter(Mandatory = $true, position = 0, ValueFromRemainingArguments = $true)]
     [String[]]$files
   )
   $folder = Read-Host "Enter folder name"
   Compress-Archive -Path "$files" -DestinationPath ("$pwd" + "\" + "$folder" + ".zip")
- }
-
+}
+    
 function unzip ($file, $folder) {
-if (!$file){ $file = Read-Host "Enter zip file name" }
-if (!$folder){ $folder = Read-Host "Enter folder name" }
-
-Expand-Archive -Path "$file" -DestinationPath $pwd\$folder
-trash $file
-c $folder
+  if (!$file) { $file = Read-Host "Enter zip file name" }
+  if (!$folder) { $folder = Read-Host "Enter folder name" }
+      
+  Expand-Archive -Path "$file" -DestinationPath $pwd\$folder
+  trash $file
+  c $folder
 }
-
+    
 function c ($dir) {
-cd $dir 
-ls
+  cd $dir 
+  ls
 }
-
+    
 #Only display the last two directories in the prompt
 function prompthelper {
-$short_dir = $pwd.Replace('Microsoft.PowerShell.Core\FileSystem::', '')
-return $short_dir
+  $short_dir = $pwd.Replace('Microsoft.PowerShell.Core\FileSystem::', '')
+  return $short_dir
 }
-
+    
 function Prompt {
-return "$pwd`r`n> "
+  return "$pwd`r`n> "
 }
-
+    
 function f {
-Set-Location "$(find . -type d | fzf)"
+  Set-Location "$(find . -type d | fzf)"
 }
-
+    
 function findme2 {
-param (
-[string]$file,
-[string]$folderflag
-)
-if (-not ("$file")) {
-return "Please provide a valid file and directory to search for."
-}
-
-$directory = "."
-
-# Assume we're finding folder names
-$filematches = Get-ChildItem -Path "$directory" -Filter "*$file*" -Directory -recurse
-
-# ..unless the user omits the folder flag ;)
-if ($folderflag -eq "") {
-$filematches = Get-ChildItem -Path "$directory" -Filter "*$file*" -File -recurse
-}
-
-if ($filematches.Count -gt 0) {
-$results = @()
-Write-Host "Partial matches found:"
-foreach ($match in $filematches) {
-$results += $match.FullName
-}
-for ($i = 0; $i -lt $results.Length; $i++) {
-Write-Host "$i $($results[$i])"
-}
-[int]$index = Read-Host "select"
-$global:find_selection = $results[$index]
-Set-Clipboard $results[$index]
-return 1
-}
-return 0
+  param (
+    [string]$file,
+    [string]$folderflag
+  )
+  if (-not ("$file")) {
+    return "Please provide a valid file and directory to search for."
+  }
+      
+  $directory = "."
+      
+  # Assume we're finding folder names
+  $filematches = Get-ChildItem -Path "$directory" -Filter "*$file*" -Directory -recurse
+      
+  # ..unless the user omits the folder flag ;)
+  if ($folderflag -eq "") {
+    $filematches = Get-ChildItem -Path "$directory" -Filter "*$file*" -File -recurse
+  }
+      
+  if ($filematches.Count -gt 0) {
+    $results = @()
+    Write-Host "Partial matches found:"
+    foreach ($match in $filematches) {
+      $results += $match.FullName
+    }
+    for ($i = 0; $i -lt $results.Length; $i++) {
+      Write-Host "$i $($results[$i])"
+    }
+    [int]$index = Read-Host "select"
+    $global:find_selection = $results[$index]
+    Set-Clipboard $results[$index]
+    return 1
+  }
+  return 0
 }
 function findme {
-param (
-[string]$file,
-[string]$directory
-)
-if (-not (Test-Path "$directory") -or -not ("$file")) {
-return "Please provide a valid file and directory to search for."
+  param (
+    [string]$file,
+    [string]$directory
+  )
+  if (-not (Test-Path "$directory") -or -not ("$file")) {
+    return "Please provide a valid file and directory to search for."
+  }
+  $filematches = Get-ChildItem -Path "$directory" -Filter "*$file*" -File
+  if ($filematches.Count -gt 0) {
+    Write-Host "Partial matches found:"
+    foreach ($match in $filematches) {
+      Write-Host $match.FullName
+    }
+    return 1
+  }
+  return 0
 }
-$filematches = Get-ChildItem -Path "$directory" -Filter "*$file*" -File
-if ($filematches.Count -gt 0) {
-Write-Host "Partial matches found:"
-foreach ($match in $filematches) {
-  Write-Host $match.FullName
-}
-return 1
-}
-return 0
-}
-
+    
 function getname {
-  Set-Clipboard (Get-Item -Path $pwd).Name
+  Set-Clipboard ("'" + (Get-Item -Path $pwd).Name + "'")
   Write-Host "Name copied!"
 }
-
+    
 function getdir {
-Set-Clipboard $pwd
-$full_path = Get-Clipboard
-$prefix = "Microsoft.PowerShell.Core\FileSystem::"
-return $full_path.Replace($prefix, "")
+  Set-Clipboard $pwd
+  $full_path = Get-Clipboard
+  $prefix = "Microsoft.PowerShell.Core\FileSystem::"
+  return $full_path.Replace($prefix, "")
 }
-
+    
 function getpath($file) {
-if (!$file) {
-Write-Host "Error: No source file provided."
-return
+  if (!$file) {
+    Write-Host "Error: No source file provided."
+    return
+  }
+  $dir = (Get-Item $file).DirectoryName
+  $file = (Get-Item $file).Name
+  $path = Join-Path $dir $file
+  Set-Clipboard $path
 }
-$dir = (Get-Item $file).DirectoryName
-$file = (Get-Item $file).Name
-$path = Join-Path $dir $file
-Set-Clipboard $path
-}
-
+    
 function man($cmd) {
-Get-Help $cmd -full
+  Get-Help $cmd -full
 }
-
+    
 function open {
-param (
-[CmdletBinding()]
-[Parameter(Mandatory=$true, position=0, ValueFromRemainingArguments=$true)]
-[String[]]$files
-)
-Invoke-Item $files
+  param (
+    [CmdletBinding()]
+    [Parameter(Mandatory = $true, position = 0, ValueFromRemainingArguments = $true)]
+    [String[]]$files
+  )
+  Invoke-Item $files
 }
-
+    
 function mv($file, $destination) {
-Move-Item -Path $file -Destination $destination
+  Move-Item -Path $file -Destination $destination
 }
-
+    
 function touch($file) {
-"" | Out-File $file -Encoding ASCII
+  "" | Out-File $file -Encoding ASCII
 }
-
+    
 function mkdir ($dir) {
-New-Item -Path "." -Name "$dir" -ItemType "directory"
+  New-Item -Path "." -Name "$dir" -ItemType "directory"
 }
-
+    
 function ga { git add . }
 function gs { git status }
 function gc {
-param([Parameter(Mandatory=$true)] [string]$message)
-git commit
+  param([Parameter(Mandatory = $true)] [string]$message)
+  git commit
 }
 function gp { git push }
-
+    
 function right ($file) {
-  if (-not $file) { return "Please provide a file name."}
+  if (-not $file) { return "Please provide a file name." }
   pdftk $file cat 1-endeast output out.pdf
   rm $file
   Rename-Item -Path out.pdf -NewName $file
 }
-
+    
 function strip ($file) {
-  if (-not $file) { return "Please provide a file name."}
+  if (-not $file) { return "Please provide a file name." }
   pdftk $file cat output out.pdf
   rm $file
   Rename-Item -Path out.pdf -NewName $file
+}
+
+function note ($file) {
+  if (-not $file) { return "Please provide a file name." }
 }
