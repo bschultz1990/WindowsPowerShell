37c311600dd0c33f53d694c28c7938a8e3c6089d
Ben Schultz <Ben_Schultz@vfc.com>
2023-07-26 13:42:42 -0500

    Add Kipling checklist and extend the todo function.

--- custom_scripts/todo.ps1
+++ custom_scripts/todo.ps1
@@ -1,7 +1,33 @@
-function todo ($todo) {
-  while (-not $todo) { $todo = Read-Host "Todo content" }
-  Add-Content -Path todo.txt -Value $todo
-  nvim todo.txt +sort +wq
-  Clear-Host
-  bat todo.txt
+$todo_path = "C:\Users\bschul3\OneDrive - VFC Corp\Documents\todo.txt"
+
+function todo {
+  param (
+    [switch]$a,
+    [switch]$l,
+    [switch]$e
+  )
+  
+  if ($a) {
+    $todo = Read-Host "Todo content"
+    Add-Content -Path $todo_path -Value $todo
+    nvim $todo_path +sort +wq
+    Clear-Host
+    bat $todo_path
+    return
+  }
+  
+  if ($l) {
+    bat $todo_path
+    return
+  }
+
+  if ($e) {
+    nvim $todo_path
+    return
+  }
+  
+  Write-Host "todo -flag`n" +
+  "-a: add todo`n" +
+  "-l: list todos`n" +
+  "-e: edit todos file`n"
 }
