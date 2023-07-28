56bfe7bc859bdbcc470b44f2d6a4e073e61e8839
= <=>
2023-07-28 14:23:01 -0500

    Extend 'gn' to accept files. Add 'paths' to print out paths.ps1

--- custom_scripts/gn.ps1
+++ custom_scripts/gn.ps1
@@ -1,4 +1,9 @@
-function gn {
-  Set-Clipboard ((Get-Item -Path $pwd).Name)
+function gn ($file) {
+  if (-not $file) {
+    Set-Clipboard ((Get-Item -Path $pwd).Name)
     Write-Host "Name copied!"
+    return
+  }
+  Set-Clipboard ((Get-Item -Path $file).Name)
+  Write-Host "Name copied!"
 }
