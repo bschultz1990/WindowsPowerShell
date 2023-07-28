8b282e58a9ec28cb1e50ac7f5be79c9b10c33785
Ben Schultz <Ben_Schultz@vfc.com>
2023-07-28 16:35:18 -0500

    Change 'c' to be a poor man's directory changer.

--- custom_scripts/c.ps1
+++ custom_scripts/c.ps1
@@ -1,4 +1,21 @@
-function c ($dir) {
-  Set-Location $dir 
-    Get-ChildItem
+$global:dirs_array = @()
+
+function c {
+  while ($response -ne "q") {
+    # get-childitem of current dir
+    $dirs = Get-ChildItem
+    # push just the name of each item into an array
+    foreach ($dir in $dirs) {
+      $global:dirs_array += $dir.Name
+    }
+    # display array
+    for ($i = 0; $i -lt $global:dirs_array.Length; $i++) {
+      Write-Host "$i  $($global:dirs_array[$i])"
+    }
+    # ask user for array index
+    $response = Read-Host "Goto or 'q' to quit"
+    # cd to that index's directory
+    cd $global:dirs_array[$response]
+    $global:dirs_array.clear()
+  }
 }
---
+++ custom_scripts/paths-GHZL1J3.ps1
@@ -0,0 +1,12 @@
+$lfrc = "C:\Users\bschul3\AppData\Local\lf\lfrc"
+
+function paths {
+  $content = Get-Content $pwshconfig/custom_scripts/vfpaths.ps1
+  $table = [ordered]@{}
+  foreach ($line in $content) {
+    if ($line -match '(\$.+?) ') { $key = $Matches[1] }
+    if ($line -match '\"(.+?)"') { $value = $Matches[1] }
+    $table.Add($key, $value) 
+  }
+  Write-Output ($table)
+}
