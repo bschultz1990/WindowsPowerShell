9d45187568fbdc2e8974610ec23e63c3f26ac1c3
= <=>
2023-07-28 17:33:24 -0500

    Revine the 'c' function to be a better navigator.

--- custom_scripts/c.ps1
+++ custom_scripts/c.ps1
@@ -1,21 +1,22 @@
-$global:dirs_array = @()
-
 function c {
-  while ($response -ne "q") {
-    # get-childitem of current dir
-    $dirs = Get-ChildItem
-    # push just the name of each item into an array
-    foreach ($dir in $dirs) {
-      $global:dirs_array += $dir.Name
-    }
-    # display array
+  while ($true) {
+    $global:dirs_array = @('..')
+      $dirs = Get-ChildItem -Directory
+      foreach ($dir in $dirs) {
+        $global:dirs_array += $dir.Name
+      }
     for ($i = 0; $i -lt $global:dirs_array.Length; $i++) {
       Write-Host "$i  $($global:dirs_array[$i])"
     }
-    # ask user for array index
     $response = Read-Host "Goto or 'q' to quit"
-    # cd to that index's directory
-    cd $global:dirs_array[$response]
-    $global:dirs_array.clear()
+      if ($response -eq 'q') {
+        Clear-Host
+          Get-ChildItem
+          return
+      }
+
+    Set-Location $global:dirs_array[$response]
+      $global:dirs_array = @('..')
+      Clear-Host
   }
 }
