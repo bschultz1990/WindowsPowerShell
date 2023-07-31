a54786f4502c1fa295191ab3635814f4f99b8d01
Ben Schultz <Ben_Schultz@vfc.com>
2023-07-31 14:50:01 -0500

    Put back the paths functionality

--- custom_scripts/paths.ps1
+++ custom_scripts/paths.ps1
@@ -1,5 +1,12 @@
 $lfrc = "C:\Users\bschul3\AppData\Local\lf\lfrc"
 
 function paths {
-  Get-Content $pwshconfig/custom_scripts/vfpaths.ps1
+  $content = Get-Content $pwshconfig/custom_scripts/vfpaths.ps1
+  $table = [ordered]@{}
+  foreach ($line in $content) {
+    if ($line -match '(\$.+?) ') { $key = $Matches[1] }
+    if ($line -match '\"(.+?)"') { $value = $Matches[1] }
+    $table.Add($key, $value) 
+  }
+  Write-Output ($table)
 }
