e36df33075d92959c80252bd683878198ecc492b
Ben Schultz <Ben_Schultz@vfc.com>
2023-07-31 14:40:06 -0500

    Beefed up 'c' with todo notes.

--- custom_scripts/c.ps1
+++ custom_scripts/c.ps1
@@ -1,22 +1,31 @@
+# TODO: Include the following functions:
+# rn: rename
+# mkdir: mkdir 1 or more
+# cp: copy 1 or more
+# mv: move 1 or more
+# rm: remove 1 or more
+
 function c {
   while ($true) {
     $global:dirs_array = @('..')
-      $dirs = Get-ChildItem -Directory
-      foreach ($dir in $dirs) {
-        $global:dirs_array += $dir.Name
-      }
+    # $dirs = Get-ChildItem -Directory
+    $dirs = Get-ChildItem
+    foreach ($dir in $dirs) {
+      $global:dirs_array += $dir.Name
+    }
     Get-Location
-      for ($i = 0; $i -lt $global:dirs_array.Length; $i++) {
-        Write-Host "$i  $($global:dirs_array[$i])"
-      }
-    $response = Read-Host "Goto or 'q' to quit"
-      if ($response -eq 'q') {
-        Clear-Host
-          Get-ChildItem
-          return
-      }
+    for ($i = 0; $i -lt $global:dirs_array.Length; $i++) {
+      Write-Host "$i  $($global:dirs_array[$i])"
+    }
+    $response = Read-Host "Command: ? for a list"
+    if ($response -eq '?') {
+      
+    }
+    if ($response -eq 'q') {
+      return
+    }
     Set-Location $global:dirs_array[$response]
-      $global:dirs_array = @('..')
-      Clear-Host
+    $global:dirs_array = @('..')
+    Clear-Host
   }
 }
