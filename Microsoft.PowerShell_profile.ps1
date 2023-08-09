52498d2840b302aff5bd0267081c1ecac86ffffc
Ben Schultz <Ben_Schultz@vfc.com>
2023-08-09 14:35:19 -0500

    Add new profile.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
-$custom_scripts = Get-ChildItem -Path "$PSScriptRoot\custom_scripts" -Filter "*.ps1"
+$custom_scripts = "C:\Users\bschul3\Documents\WindowsPowerShell\custom_scripts"
+
+$custom_scripts = Get-ChildItem -Path "$custom_scripts" -Filter "*.ps1"
 foreach ($script in $custom_scripts) {
   . $script.FullName
 }
