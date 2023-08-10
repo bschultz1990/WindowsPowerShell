44ba43cf9b80d1639aad34c6e39fc431e9bd7b41
Ben Schultz <Ben_Schultz@vfc.com>
2023-08-10 10:16:24 -0500

    Add a remote.ps1 file which can be sourced and synced.

--- remote.ps1
+++ remote.ps1
@@ -1,3 +1,7 @@
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
-irm get.scoop.sh | iex
-scoop bucket add exras
+foreach ($script in (Get-Item "\\Client\C$\Users\bschul3\Documents\WindowsPowerShell\custom_scripts\*.ps1")) {
+        . $script.FullName
+        }
+
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+# irm get.scoop.sh | iex
+# scoop bucket add exras
