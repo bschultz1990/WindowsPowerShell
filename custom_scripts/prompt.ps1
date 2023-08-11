a4c59c888971b8504b18d4f4d1ece7f0a7f1fa26
= <=>
2023-08-11 17:15:04 -0500

    Add git status in prompt

--- custom_scripts/prompt.ps1
+++ custom_scripts/prompt.ps1
@@ -1,4 +1,13 @@
+function gitstatus {
+
+}
+
 # https://devblogs.microsoft.com/powershell/fun-with-paths/
 function Prompt {
-  return "$((Get-Item .).fullname)`r`n> "
+  $base = "$((Get-Item .).fullname)"
+
+  if (git symbolic-ref --short HEAD 2>$null) { $notation = " " + (git symbolic-ref --short HEAD 2>$null) }
+  if (git status -s 2>$null) { $notation += " +" + (git status -s 2>$null | Measure-Object -Line).Lines }
+
+  return $base + $notation + "`r`n> "
 }
