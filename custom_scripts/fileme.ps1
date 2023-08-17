fad9bed22fbd26a7d4996aac6dbc1f51df0e617d
Ben Schultz <Ben_Schultz@vfc.com>
2023-08-17 16:45:16 -0500

    Made fileme STRONGERER!

--- custom_scripts/fileme.ps1
+++ custom_scripts/fileme.ps1
@@ -1,7 +1,11 @@
 function fileme {
 	$confirmed = $false
 	while ($confirmed -eq $false) {
-		$response = Read-Host "Have you made sure there are no Starlike invoices that are USDC? (yes/no)"
+		Write-Host "=== IMPORTANT! Complete all of the following before continuing:===`n"
+    "[ ] Remove all Starlike orders that are USDC`n"
+
+    $response = Read-Host "Have you completed all of the above? (yes/no)"
+
 		if ($response.ToLower() -eq 'no') {
 			return
 		}
@@ -12,7 +16,26 @@ function fileme {
 			Write-Host "Invalid input. Please enter 'yes' or 'no'."
 		}
 	}
-	Write-Host "Archiving VANS Forus to $vans_forus"; Move-Item *VN*FORUS*.pdf $vans_forus
-	Write-Host "Archiving TNF Komax SA (Chile) to $tnf_komax_chile"; Move-Item *KOMAX*SA*.pdf $tnf_komax_chile
-	Write-Host "Moving SW Starlike to $smartwool_starlike"; Move-Item *SW*STARLIKE*.pdf $smartwool_starlike
+	Write-Host "Archiving VANS Forus to $vans_forus"
+  Move-Item "*VN*FORUS*.pdf" $vans_forus
+	
+  Write-Host "Archiving TNF Komax SA (Chile) to $tnf_komax_chile"
+  Move-Item "*KOMAX*SA *.pdf" $tnf_komax_chile
+
+	Write-Host "Archiving TNF Komax Peru to $tnf_komax_peru"
+  Move-Item "*KOMAX PERU*.pdf" $tnf_komax_peru
+
+	Write-Host "Archiving SW Starlike to $smartwool_starlike"
+  Move-Item *SW*STARLIKE*.pdf $smartwool_starlike
+
+  Write-Host "Moving Lost Arrow to $smartwool_lostarrow_edit"
+  Move-Item "*LOST ARROW*.pdf" $smartwool_lostarrow_edit
+
+  Write-Host "Moving VANS Grimuru to $vans_grimuru_edit"
+  Move-Item *VN*GRIMURU*.pdf $vans_grimuru_edit
+  folderme $vans_grimuru_edit vans grimuru
+
+  Write-Host "Moving TNF COMERCIAL to $tnf_comercial_edit"
+  Move-Item *TNF*IMPORTADORA*.pdf $tnf_comercial_edit
+  folderme $tnf_comercial_edit tnf comercial
 }
--- custom_scripts/folderme.ps1
+++ custom_scripts/folderme.ps1
@@ -1,72 +1,80 @@
 function folderme {
   param (
-    [string]$FolderPath,
-    [string]$company,
-    [string]$brand
-  )
-  if (-not ("$FolderPath")) {
-    $FolderPath = "."
-  }
+      [string]$FolderPath,
+      [string]$company,
+      [string]$brand
+      )
+    if (-not ("$FolderPath")) {
+      $FolderPath = "."
+    }
   Write-Host "Processing in $FolderPath"
-  if (-not ("$company")) {
-    $company = Read-Host "Company"
-  }
+    if (-not ("$company")) {
+      $company = Read-Host "Company"
+    }
   if (-not ("$brand")) {
     $brand = Read-Host "Brand"
   }
 
   $company = $company.ToLower()
-  $brand = $brand.ToLower()
+    $brand = $brand.ToLower()
 
-  $tnf = @{
-    komax = @{
-      prefix = 'TNF '
-      suffix = ' KOMAX'
+    $tnf = @{
+      comercial = @{
+        prefix = 'TNF '
+        suffix = ' TNF COMERCIAL'
+        }
+      komax_chile = @{
+        prefix = 'TNF '
+          suffix = ' KOMAX SA'
+      }
+      komax_peru = @{
+        prefix = 'TNF'
+          suffix = ' KOMAX PERU'
+      }
     }
-  }
   $smartwool = @{
     starlike = @{
       prefix = 'SW '
-      suffix = ' STARLIKE'
+        suffix = ' STARLIKE'
     }
   }
   $vans = @{
     grimuru = @{
       prefix = 'VN '
-      suffix = ' GRIMURU'
+        suffix = ' GRIMURU'
     }
     forus   = @{
       prefix = 'VN '
-      suffix = ' FORUS'
+        suffix = ' FORUS'
     }
   }
 
   $files = Get-ChildItem -Path $FolderPath -Filter *.pdf -File
-  $prefix = (Get-Variable -Name $company -ValueOnly).$brand.prefix
-  $suffix = (Get-Variable -Name $company -ValueOnly).$brand.suffix
-  $regex = $prefix, "(.+?)", $suffix -join ""
-  # Write-Host "regex is: $regex"
+    $prefix = (Get-Variable -Name $company -ValueOnly).$brand.prefix
+    $suffix = (Get-Variable -Name $company -ValueOnly).$brand.suffix
+    $regex = $prefix, "(.+?)", $suffix -join ""
+# Write-Host "regex is: $regex"
 
-  $uniqueMatches = @{}  # create hashtable to store unique matches
+    $uniqueMatches = @{}  # create hashtable to store unique matches
 
-  foreach ($file in $files) {
-    if ($file.Name -match "$regex") {
-      $newFolderName = $Matches[1], "_NEW" -join ""
-      $newFolderPath = Join-Path -Path $FolderPath -ChildPath $newFolderName
+    foreach ($file in $files) {
+      if ($file.Name -match "$regex") {
+        $newFolderName = $Matches[1], "_NEW" -join ""
+          $newFolderPath = Join-Path -Path $FolderPath -ChildPath $newFolderName
 
-      # check if folder name already exists in hashtable
-      if ($uniqueMatches.ContainsKey($newFolderName)) {
-        $newFolderPath = $uniqueMatches[$newFolderName]  # use existing folder path
+# check if folder name already exists in hashtable
+          if ($uniqueMatches.ContainsKey($newFolderName)) {
+            $newFolderPath = $uniqueMatches[$newFolderName]  # use existing folder path
+          }
+          else {
+# create new folder and add to hashtable
+            if (-not (Test-Path -Path $newFolderPath)) {
+              New-Item -ItemType Directory -Path $newFolderPath | Out-Null
+                Write-Host $newFolderName
+            }
+            $uniqueMatches[$newFolderName] = $newFolderPath
+          }
+        Move-Item -Path $file.FullName -Destination $newFolderPath
       }
-      else {
-        # create new folder and add to hashtable
-        if (-not (Test-Path -Path $newFolderPath)) {
-          New-Item -ItemType Directory -Path $newFolderPath | Out-Null
-          Write-Host $newFolderName
-        }
-        $uniqueMatches[$newFolderName] = $newFolderPath
-      }
-      Move-Item -Path $file.FullName -Destination $newFolderPath
     }
-  }
 }
