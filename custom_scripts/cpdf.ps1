a3a8028119fd7c2f4c351a97f31a6ac35c817b7b
Ben Schultz <Ben_Schultz@vfc.com>
2023-10-17 15:50:02 -0500

    Move custom path additions to vfpaths.ps1
    Also cleaned up cpdf in preparation for the Arezzo weight function.

--- custom_scripts/cpdf.ps1
+++ custom_scripts/cpdf.ps1
@@ -1,3 +1,24 @@
-$env:Path += ";C:\Users\bschul3\Apps\"
-# cpdf -add-text "80.00 kg" -topleft "319 239" -font "Helvetica" -font-size 8 '.\VN V000103257 AREZZO INDUSTRIA E COMERCIO S A 3525131272 stamped.pdf' AND -add-text "90.00 kg" -topleft "442 239" -font "Helvetica" -font-size 8 '.\VN V000103257 AREZZO INDUSTRIA E COMERCIO S A 3525131272 stamped.pdf' -o out.pdf
+. $PSScriptRoot\vfpaths.ps1
+. $PSScriptRoot\fileme.ps1
 
+# cpdf -add-text "80.00 kg" -topleft "319 239" -font "Helvetica" -font-size 8 '.\VN V000103257 AREZZO INDUSTRIA E COMERCIO S A 3525131272 stamped.pdf' AND -add-text "90.00 kg" -topleft "442 239" -font "Helvetica" -font-size 8 '.\VN V000103257 AREZZO INDUSTRIA E COMERCIO S A 3525131272 stamped.pdf' -o out.pdf
+# -match '(?<left>\d+)(?<decimal>,*\.*)(?<right>\d*)?'
+
+function invoiceNums {
+  # if the backup directory doesn't exist, create one.
+  if (-not (Test-Path backup)) {
+    mkdir backup
+  }
+  Copy-Item *.pdf "./backup"
+
+  # Ask the user for a list of invoice numbers. Compare it to the files in the present directory.
+  $inv = Read-Host "Enter list of invoice numbers"
+  $inv = $inv.Split(";")
+  $global:ivalues = @()
+  foreach ($i in $inv) {
+    $global:ivalues += $i
+  }
+  Write-Host $global:ivalues
+  Write-Host "Type: " -NoNewline
+  Write-Host $global:ivalues.GetType().Name -ForegroundColor Green
+}
