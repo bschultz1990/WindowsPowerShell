c7fe40d7e2305c1ebd0555455ed1e03d850a38ea
Ben Schultz <Ben_Schultz@vfc.com>
2023-10-20 15:29:06 -0500

    Add more logic in cpdf. Now we have a full object with net and gross weights.

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-$custom_scripts = "~\Documents\WindowsPowerShell\custom_scripts"
-
-$custom_scripts = Get-ChildItem -Path "$custom_scripts" -Filter "*.ps1"
-foreach ($script in $custom_scripts) {
-  . $script.FullName
-}
--- custom_scripts/cpdf.ps1
+++ custom_scripts/cpdf.ps1
@@ -5,49 +5,109 @@
 # -match '(?<left>\d+)(?<decimal>,*\.*)(?<right>\d*)?'
 
 function invoiceNums {
-	# # if the backup directory doesn't exist, create one.
-	# if (-not (Test-Path backup)) {
-	#   $null = mkdir backup
-	# }
-	# Copy-Item *.pdf "./backup"
-	
-	# Ask the user for a list of invoice numbers. Compare it to the files in the present directory.
-	$inv = Read-Host "Enter list of invoice numbers separated by a semicolon"
-	$global:inv = $inv.Split(";")
-	Write-Host "Checking for invoice numbers..." -ForegroundColor DarkGray
-	foreach ($i in $global:inv) {
-		Write-Host "$i" -NoNewline
-		if (-not (Test-Path -Path *$i* -PathType leaf)) {
-			Write-Host " Not Found! " -ForegroundColor Red
-		}
-		if (Test-Path -Path *$i* -PathType leaf) {
-			Write-Host " Found! " -ForegroundColor Green -NoNewline
-			Write-Host (Get-Item *$i*).Name -ForegroundColor DarkGray
-		}
-	}
-	Write-Host ""
-	# $netWtString = Read-Host "Enter net weights for the invoices above in order, separated by a semicolon. e.g. 80.00;90.00"
-	# $global:netWt = $netWtString.Split(";")
+  $global:invoiceTable = [ordered]@{}
 
-	# # Copy to a local variable so we can work with it inside the loop
-	# $tempNetWt = @()
-	# foreach ($i in $global:netWt) {
-	# 	$tempNetWt += $i
-	# }
-	# for ($i = 0; $i -lt $netWt.Length; $i++) {
-	# 	# replace commas with decimal points
-	# 	$tempNetWt[$i] = $global:netWt[$i].Replace(",", ".")
+  # Ask the user for a list of invoice numbers. Compare it to the files in the present directory.
+  $inv = Read-Host "Enter list of invoice numbers separated by a semicolon or 'q' to quit."
+  if ($inv.ToLower() -eq "q") {
+    return	
+  }
+  $global:inv = $inv.Split(";")
+  Write-Host "Checking for invoice numbers..." -ForegroundColor DarkGray
+  foreach ($i in $global:inv) {
+    Write-Host "$i" -NoNewline
 
-	# 	# convert each weight to a number with two decimal places
-	# 	$tempNetWt[$i] = "{0:F2}" -f [float]$global:netWt[$i]
+    if (-not (Test-Path -Path *$i*.pdf -PathType leaf) -or $i -eq "") {
+      Write-Host " Not Found! " -ForegroundColor Red
+      return Write-Host "One or more invoices were not found. Double-check your files and/or omit a semicolon from the end." -ForegroundColor Red
+    }
 
-	# 	# append kg
-	# 	$tempNetWt[$i] += " kg"
-		
-	# }
-	# $global:netWt = $tempNetWt
-	# for ($j = 0; $j -lt $global:netWt.Length; $j++) {
-	# 	Write-Host $global:netWt[$j] " "-ForegroundColor DarkGray -NoNewline
-	# 	Write-Host $global:netWt[$j].GetType() -ForegroundColor Yellow
-	# }
+    if (Test-Path -Path *$i*.pdf -PathType leaf) {
+      Write-Host " Found! " -ForegroundColor Green -NoNewline
+      Write-Host (Get-Item *$i*).Name -ForegroundColor DarkGray
+      # Add an inner ordered dictionary with invoice number for later
+      $global:invoiceTable["$i"] = [ordered]@{
+        "Net Wt."   = $null
+        "Gross Wt." = $null
+      }
+    }
+  }
+  Write-Host ""
+
+  $netWtString = Read-Host "Enter net weights separated by a semicolon or press 'q' to quit."
+  if ($netWtString.ToLower() -eq "q") {
+    return
+  }
+  # Split at ; and remove blanks
+  $tempNetWt = $null
+  $tempNetWt = $netWtString.Split(";")
+
+  foreach ($i in $tempNetWt) {
+    $i = $i.Replace(",", ".")
+    $i = "{0:F2}" -f [float]$i
+    $i += " kg"
+  }
+
+  $global:netWt = $tempNetWt
+  Write-Host ""
+
+  $grossWtString = Read-Host "Enter gross weights separated by a semicolon or 'q' to quit."
+  if ($grossWtString.ToLower() -eq "q") {
+    return
+  }
+  # Split at ; and remove blanks
+  $tempGrossWt = $null
+  $tempGrossWt = $grossWtString.Split(";")
+
+  foreach ($i in $tempGrossWt) {
+    $i = $i.Replace(",", ".")
+    $i = "{0:F2}" -f [float]$i
+    $i += " kg"
+  }
+
+  $global:grossWt = $tempGrossWt
+
+  # Abort if net weight and gross weight length are not equal.
+  if ($global:netWt.Length -ne $global:grossWt.Length) {
+    Write-Host "Aborted. You must have the same number of net weights and gross weights. See below for details:" -ForegroundColor Red
+    Write-Host -NoNewline "Net Wt. Entries: "
+    Write-Host $global:netWt.Length -ForegroundColor DarkGray
+    for ($i = 0; $i -lt $global:netWt.Length; $i++) {
+      Write-Host "$i $($global:netWt[$i])"
+    }
+
+    Write-Host -NoNewline "Gross Wt. "
+    Write-Host $global:grossWt.Length -ForegroundColor DarkGray
+    for ($i = 0; $i -lt $global:grossWt.Length; $i++) {
+      Write-Host "$i $($global:grossWt[$i])"
+    }
+    return
+  }
+
+  # Assign values to invoiceTable:
+  for ($j = 0; $j -lt $global:netWt.Length; $j++) {
+    $invoiceNumber = $global:inv[$j]
+    $global:invoiceTable[$invoiceNumber]["Net Wt."] = $global:netWt[$j]
+    $global:invoiceTable[$invoiceNumber]["Gross Wt."] = $global:grossWt[$j]
+  }
+
+  # Display the results and ask for confirmation before continuing.
+  Write-Host ""
+  Write-Host "Invoice Table:"
+  Write-Host ""
+  Write-Host "Invoice Number Net Wt. Gross Wt."
+  foreach ($i in $global:invoiceTable.Keys) {
+    Write-Host "$i" -NoNewLine
+    Write-Host " $($global:invoiceTable[$i]["Net Wt."])" -NoNewline
+    Write-Host " $($global:invoiceTable[$i]["Gross Wt."])"
+  }
+  $response = Read-Host "Review carefully. Is this correct? [yes][no]"
+  if ($response.ToLower() -eq "no") {
+    return Write-Host "Aborted. Please try again with correct data." -ForegroundColor Yellow
+  }
+
+  # ACTIVATE FORWARD CPDF FIREPOWER!
+  for ($i = 0; $i -lt $global:inv.Length; $i++) {
+    Write-Host $global:inv[$i]
+  }
 }
