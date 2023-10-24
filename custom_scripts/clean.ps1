c405f7f6575fa916cba1958d4da4fab9593f2e97
Ben Schultz <Ben_Schultz@vfc.com>
2023-10-24 11:54:41 -0500

    Added cleaning of /Attachments/* folder in 'clean.ps1'

--- custom_scripts/clean.ps1
+++ custom_scripts/clean.ps1
@@ -1,9 +1,10 @@
 # Start-Job -ScriptBlock {
 function clean {
   clear
-  Write-Host "Cleaning ~/Downloads..."
+  Write-Host "Cleaning ~/Downloads and OneDrive Attachments..."
   while ($true) {
     Remove-item ~/Downloads/*.ica
+    Remove-item ~/OneDrive - VFC Corp/Attachments/*
     Start-Sleep -Seconds 60
   }
 }
