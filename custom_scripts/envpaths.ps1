7fbc93ee303b655c991da93c237a5886fe1da8d8
Ben Schultz <Ben_Schultz@vfc.com>
2023-10-31 15:50:49 -0500

    Add python to path.

--- custom_scripts/envpaths.ps1
+++ custom_scripts/envpaths.ps1
@@ -1,3 +1,5 @@
 $env:Path += ";C:\Users\bschul3\Apps\"
 $env:Path += ";C:\Program Files\Microsoft Office\root\Office16"
 $env:Path += ";C:\Program Files\Adobe\Acrobat DC\Acrobat"
+$env:Path += ";C:\Program Files (x86)\Foxit Software\Foxit PDF Editor"
+$env:Path += ";C:\Users\bschul3\Documents\python"
