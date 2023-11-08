63108c2444926ad2f206b911dd37b5d60e20766e
Ben Schultz <Ben_Schultz@vfc.com>
2023-11-08 11:40:08 -0600

    Add path for winsetview

--- custom_scripts/envpaths.ps1
+++ custom_scripts/envpaths.ps1
@@ -3,3 +3,5 @@ $env:Path += ";C:\Program Files\Microsoft Office\root\Office16"
 $env:Path += ";C:\Program Files\Adobe\Acrobat DC\Acrobat"
 $env:Path += ";C:\Program Files (x86)\Foxit Software\Foxit PDF Editor"
 $env:Path += ";C:\Users\bschul3\Documents\python"
+$env:Path += ";C:\Users\bschul3\Apps\WinSetView-main"
+
