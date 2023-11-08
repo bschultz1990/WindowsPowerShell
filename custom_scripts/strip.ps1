c78ce49a7291efb596dc096606784dd7d4c6de7a
Ben Schultz <Ben_Schultz@vfc.com>
2023-11-08 11:23:45 -0600

    Made strip.ps1 silent CORRECTLY.

--- custom_scripts/strip.ps1
+++ custom_scripts/strip.ps1
@@ -3,10 +3,11 @@ function strip ($file) {
   if ([System.IO.Path]::GetExtension($file) -ne '.pdf') {
     return "Please provide a pdf file."
   }
-  # if (-not (Test-Path -Path backup)) {
-  #   $null = mkdir backup
-  # }
+  if (-not (Test-Path -Path backup)) {
+    $null = mkdir backup
+  }
   cpdf $file -o out.pdf
-  # Move-Item $file backup
+  Move-Item $file backup
   Rename-Item -Path out.pdf -NewName $file
+  rm backup -Recurse
 }
