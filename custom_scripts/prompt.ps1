edd0b6e857720e1cee77bac4c5712ead32c10b41
Ben Schultz <ben@schultz.com>
2024-06-10 15:16:50 -0500

    Prune the prompt.ps1

--- custom_scripts/prompt.ps1
+++ custom_scripts/prompt.ps1
@@ -1,17 +1,4 @@
-function gitstatus {
-
-}
-
 # https://devblogs.microsoft.com/powershell/fun-with-paths/
-# function Prompt {
-#   $base = "$((Get-Item .).fullname)"
-
-#   if (git symbolic-ref --short HEAD 2>$null) { $notation = " " + (git symbolic-ref --short HEAD 2>$null) }
-#   if (git status -s 2>$null) { $notation += " +" + (git status -s 2>$null | Measure-Object -Line).Lines }
-
-#   return $base + $notation + "`r`n> "
-# }
-
 function Prompt {
   $base = "$((Get-Item .).fullname)"
   return $base + "`r`n> "
