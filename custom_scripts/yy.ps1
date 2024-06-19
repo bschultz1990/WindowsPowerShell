5a405e696c469e53835216f048ef8ba63235ee23
= <=>
2024-06-18 19:03:25 -0500

    Clean up paths for Yazi

---
+++ custom_scripts/yy.ps1
@@ -0,0 +1,9 @@
+﻿function yy {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
