6ac45effbfc129cb7741d2d83226733b0cb337db
Ben Schultz <ben@schultz.com>
2024-08-01 16:29:30 -0500

    Add more invoice stamp goodness

--- custom_scripts/cc.ps1
+++ custom_scripts/cc.ps1
@@ -1,13 +1,21 @@
 function cc {
+  $filecounter = 0
   ls $dl
+  foreach ($file in (Get-ChildItem $dl)){
+    $filecounter += 1
+  }
   Write-Host
+  Write-Host $filecounter "files"
   $response = Read-Host "Continue? [y, n]"
   while ($response.ToLower() -notin @('y', 'n')) {
-    $response = Read-Host "Correct responses are either 'Y' or 'N': "
+    $response = Read-Host "Correct responses are either 'y' or 'n': "
   }
   if ($response.ToLower() -eq "y") {
     rm *.pdf
     python "C:\Users\bschul3\OneDrive - VFC Corp\Documents\international_invoices.py"
+    #foreach ($invoice in (Get-ChildItem *stamped.pdf)) {
+    #  cpdf -stamp-on "C:\Users\bschul3\OneDrive - VFC Corp\Documents\watermarks_custom\Clear Weights.pdf" $invoice -o $invoice
+    #}
     mv $dl/*stamped* .
     rm $dl/*.pdf
     ls $dl
