6b54b725f72352eacb04ba5cbc8fc329d14df999
= <=>
2024-08-27 12:27:07 -0500

    Add gh commit to notes

--- custom_scripts/notes.ps1
+++ custom_scripts/notes.ps1
@@ -1,6 +1,19 @@
-function notes {
-  Set-Location ~\Documents\notes
+function notes
+{
+  #TODO: Add a list of programs. Loop through them and check to see if they exist.
+  #If not, display an error message like the one below with the program's name.
+  if (!(Get-Command -Name gh -ErrorAction SilentlyContinue))
+  {
+    Write-Host "'gh' is not available. Please install 'gh' and try again."
+    exit
+  }
+  if (!( Test-Path ~\Documents\notes))
+  {
+    Set-Location ~\Documents
+    gh repo clone notes
+  }
+  else { Set-Location ~\Documents\notes }
+
   git pull
-  #nvim +"Telescope live_grep""
   nvim +"Telescope find_files"
 }
